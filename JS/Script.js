b2c2c6985df21e16d5d28065ac80ce099633a698
pauldv-coder <83824766+pauldv-coder@users.noreply.github.com>
2023-07-30 19:36:12 -0500

    ajuste del scroll para que se desaparezca cuando baja y aparezca cuando sube

--- JS/Script.js
+++ JS/Script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const nav = document.querySelector('#nav');
     const menu = document.querySelector('#menu');
     const close = document.querySelector('#close');
-
+    
     menu.addEventListener("click", () => {
         nav.classList.add("visible");
     });
@@ -10,4 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
     close.addEventListener("click", () => {
         nav.classList.remove("visible");
     });
+
+    
 });
+
+
+function handleScroll() {
+    
+    const header = document.getElementById("header");
+  
+    
+    if (header) {
+      
+      const currentScrollPos = window.scrollY;
+  
+      if (prevScrollPos > currentScrollPos) {
+        header.style.top = "0";
+      } else {
+        header.style.top = `-${header.offsetHeight}px`;
+      }
+  
+      prevScrollPos = currentScrollPos;
+    }
+  }
+  
+    let prevScrollPos = window.scrollY;
+  
+  window.addEventListener("scroll", handleScroll);
+  
